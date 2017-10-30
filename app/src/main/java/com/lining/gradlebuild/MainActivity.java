b4f730c6cdbc76c01647ff95156fb6ea6cf5119b
lining <liningrla@163.com>
2017-10-30 18:55:43 +0800

    添加AOP库

--- app/src/main/java/com/lining/gradlebuild/MainActivity.java
+++ app/src/main/java/com/lining/gradlebuild/MainActivity.java
@@ -1,8 +1,20 @@
 package com.lining.gradlebuild;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,9 +28,88 @@ public class MainActivity extends AppCompatActivity {
         Map<String, String> map = new HashMap<String, String>();
         map.put("lining","lining");
         testLog("testLog", map);
+        getPermission();
     }
 
     private void testLog(String param, Map<String, String> map){
         Log.e("lining","----------------------testLog");
     }
+
+    private void getPhoneNum(){
+        TelephonyManager tm = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+        String deviceid = tm.getDeviceId();//获取智能设备唯一编号
+        String te1  = tm.getLine1Number();//获取本机号码
+        Log.e("lining","----------------------te1 = "+ te1);
+    }
+
+    // 要申请的权限
+    private String[] permissions = {Manifest.permission.READ_PHONE_STATE};
+    private void getPermission(){
+        // 版本判断。当手机系统大于 23 时，才有必要去判断权限是否获取
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            // 检查该权限是否已经获取
+            int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+            // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+            if (i != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, 321);
+            }else{
+                getPhoneNum();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.e("lining","onRequestPermissionsResult--------------------");
+        if (requestCode == 321) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    // 判断用户是否 点击了不再提醒。(检测该权限是否还可以申请)
+                    boolean b = shouldShowRequestPermissionRationale(permissions[0]);
+                    if (!b) {
+                        // 用户还是想用我的 APP 的
+                        // 提示用户去应用设置界面手动开启权限
+                        goToAppSetting();
+                    } else{
+                        Toast.makeText(this, "用户还是不想用我的 APP 的", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    getPhoneNum();
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("lining","onActivityResult--------------------");
+        if (requestCode == 123) {
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 检查该权限是否已经获取
+                int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+                // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+                if (i != PackageManager.PERMISSION_GRANTED) {
+                    // 提示用户应该去应用设置界面手动开启权限
+                    // 提示用户去应用设置界面手动开启权限
+                    goToAppSetting();
+                } else {
+                    getPhoneNum();
+                    Toast.makeText(this, "权限获取成功", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    // 跳转到当前应用的设置界面
+    private void goToAppSetting() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivityForResult(intent, 123);
+    }
+
 }
